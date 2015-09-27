f32625b50da9c4029a86419a0d983505a84e93a4
a-premkumar <premkumar.alexisjegannathan@one.verizon.com>
2015-09-28 03:46:51 +0530

    MongoDB support added
    MongoDB support is added for testing purpose only. Thus created a new
    branch and not want to disturb the stable master branch.

--- src/com/proanalysis/logic/Sample.java
+++ src/com/proanalysis/logic/Sample.java
@@ -1,11 +1,20 @@
 package com.proanalysis.logic;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import com.mongodb.client.FindIterable;
 
 /**
  * Servlet implementation class Sample
@@ -25,9 +34,17 @@ public class Sample extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@SuppressWarnings("deprecation")
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		MongoClientURI uri = new MongoClientURI("mongodb://cloudfoundry-test_2p6otl8c_841b7q4b_tmtlqeaa:eb5d00ac-2a4f-4beb-80ad-9da11cff5a70@ds027908.mongolab.com:27908/cloudfoundry-test_2p6otl8c_841b7q4b");
+		MongoClient mongoClient = new MongoClient(uri);
+		MongoDatabase db = mongoClient.getDatabase(uri.getDatabase());
+		//db.getName();
+		
+		//FindIterable<Document> iterable = db.getCollection("restaurants").find(new Document("borough", "Manhattan"));
+		response.getWriter().write(db.getName());
 	}
 
 	/**
